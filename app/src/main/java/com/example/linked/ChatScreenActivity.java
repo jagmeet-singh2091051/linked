da120691e755283c12eaa73fee58cf140ad7b859
Amanpreet Singh <36414846+AmanGujral@users.noreply.github.com>
2020-12-04 16:30:59 -0500

    Created ChatScreenActivity.java

---
+++ app/src/main/java/com/example/linked/ChatScreenActivity.java
@@ -0,0 +1,15 @@
+package com.example.linked;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ChatScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatScreen);
+    }
+}
